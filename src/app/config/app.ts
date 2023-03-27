e933f79cefd243f736687841a53af4a4b0533119
alan1606 <mauri.ar3@gmail.com>
2023-03-26 19:36:39 -0600

    Cambio de dominio y redirección hacia /ris/ en base href

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,5 +1,5 @@
 
-export const BASE_ENDPOINT = 'https://diagnocons.ga:4300/api'
+export const BASE_ENDPOINT = 'https://ris.diagnocons.com:4300/api'
 //export const BASE_ENDPOINT = 'http://localhost:8090/api'
 
 export const VIEWER = 'http://ns1.diagnocons.com/sistema/viewer.php/viewer';
@@ -10,5 +10,5 @@ export const ZIP_STUDIES_PATH = 'https://ns1.diagnocons.com:8443/dcm4chee-arc/ae
 export const WEASIS_VIEWER_PATH= 'https://ns1.diagnocons.com:8443/weasis-pacs-connector/weasis?studyUID='
 export const DOWNLOAD_WEASIS_WINDOWS_LINK = 'https://github.com/nroduit/Weasis/releases/download/v3.7.0/Weasis-3.7.0-x86-64.msi';
 export const DOWNLOAD_WEASIS_MAC_LINK = 'https://github.com/nroduit/Weasis/releases/download/v3.7.0/Weasis-3.7.0.pkg';
-export const RESULTS_URL = 'https://diagnocons.ga:4222/ris/resultados/';
+export const RESULTS_URL = 'https://ris.diagnocons.com:4222/ris/resultados/';
 export const DIRECCION_CORREO_CONS = 'diagnocons@gmail.com';
