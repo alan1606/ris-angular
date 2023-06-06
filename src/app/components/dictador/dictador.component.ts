106f8cbeac31656ee4b86be43608a966e955486c
alan1606 <mauri.ar3@gmail.com>
2023-06-06 12:14:18 -0600

    Avance en la plantilla de cardio

--- src/app/components/dictador/dictador.component.ts
+++ src/app/components/dictador/dictador.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { DOWNLOAD_WEASIS_MAC_LINK, DOWNLOAD_WEASIS_WINDOWS_LINK, VIEWER, WEASIS_VIEWER_PATH, ZIP_STUDIES_PATH, FILES_PATH, BASE_ENDPOINT } from '../../config/app';
+import {
+  DOWNLOAD_WEASIS_MAC_LINK,
+  DOWNLOAD_WEASIS_WINDOWS_LINK,
+  VIEWER,
+  WEASIS_VIEWER_PATH,
+  ZIP_STUDIES_PATH,
+  FILES_PATH,
+  BASE_ENDPOINT,
+} from '../../config/app';
 import { Multimedia } from '../../models/multimedia';
 
 import { VentaConceptos } from '../../models/venta-conceptos';
@@ -13,32 +21,28 @@ import Swal from 'sweetalert2';
 import { SendMailService } from '../../services/send-mail.service';
 declare const webkitSpeechRecognition: any;
 
-import {FormControl, FormGroup} from '@angular/forms'
+import { FormControl, FormGroup } from '@angular/forms';
 
-import Quill from 'quill'
-import BlotFormatter from 'quill-blot-formatter'
+import Quill from 'quill';
+import BlotFormatter from 'quill-blot-formatter';
 import { Interpretacion } from 'src/app/models/interpretacion';
 
-Quill.register('modules/blotFormatter', BlotFormatter)
+Quill.register('modules/blotFormatter', BlotFormatter);
 
 @Component({
   selector: 'app-dictador',
   templateUrl: './dictador.component.html',
-  styleUrls: ['./dictador.component.scss']
+  styleUrls: ['./dictador.component.scss'],
 })
-
-
 export class DictadorComponent implements OnInit {
-
   interpretacion: Interpretacion;
-  enlacePdf: string = "";
-
+  enlacePdf: string = '';
 
   estudio: VentaConceptos;
   antecedentes: string = '';
   multimedia: Multimedia[] = [];
   multimediaCargada: Promise<Boolean>;
-  estudiosDeOrden: VentaConceptos[]
+  estudiosDeOrden: VentaConceptos[];
 
   mostrarSubidaExterna: boolean = true;
   medicoLocal: boolean = false;
@@ -47,52 +51,54 @@ export class DictadorComponent implements OnInit {
 
   templateForm: FormGroup;
 
-
   quillEditorModules = {};
 
-
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private ventaConceptosService: VentaConceptosService,
     private antecedenteEstudioService: AntecedenteEstudioService,
     private router: Router,
     private interpretacionService: InterpretacionService,
     private multimediaService: MultimediaService,
-    private mailService: SendMailService) {
-      this.templateForm = new FormGroup({
-        textEditor: new FormControl(""),
-      });
+    private mailService: SendMailService
+  ) {
+    this.templateForm = new FormGroup({
+      textEditor: new FormControl(''),
+    });
 
-      this.quillEditorModules = {
-        blotFormatter: {}
-      }
+    this.quillEditorModules = {
+      blotFormatter: {},
+    };
   }
 
   ngOnInit(): void {
-
-    this.route.paramMap.subscribe(params => {
-      const idVentaConcepto: number = + params.get('idVentaConcepto');
+    this.route.paramMap.subscribe((params) => {
+      const idVentaConcepto: number = +params.get('idVentaConcepto');
       if (idVentaConcepto) {
-        this.ventaConceptosService.ver(idVentaConcepto).subscribe(estudio => {
-          this.estudio = estudio;
-          this.cargarAntecedentesInicial();
-          this.cargarMultimedia();
-          this.cargarEstudiosDeOrden();
-          this.medicoLocal = this.estudio.medicoRadiologo.local;
-          this.mostrarSubidaExterna = !this.medicoLocal;
-          console.log(estudio);
-        }, error => {
-          this.router.navigate(['/']);
-        });
+        this.ventaConceptosService.ver(idVentaConcepto).subscribe(
+          (estudio) => {
+            this.estudio = estudio;
+            this.cargarAntecedentesInicial();
+            this.cargarMultimedia();
+            this.cargarEstudiosDeOrden();
+            this.medicoLocal = this.estudio.medicoRadiologo.local;
+            this.mostrarSubidaExterna = !this.medicoLocal;
+            console.log(estudio);
+            this.cargarPlantillaCardio();
+          },
+          (error) => {
+            this.router.navigate(['/']);
+          }
+        );
       }
     });
-
   }
 
   cargarPlantillaCardio(): void {
-
-    /*
+    
     console.log(this.antecedentes);
-    const jsonDoc = (toDoc(`<strong>Antecedentes:</strong> ${this.antecedentes}
+    this.templateForm.get('textEditor').setValue(
+    `<strong>Antecedentes:</strong> ${this.antecedentes}
     <br><br>
     <strong>*RITMO Y MEDICIONES*</strong><br>
 
@@ -108,49 +114,60 @@ export class DictadorComponent implements OnInit {
     <strong>INTERPRETACION:</strong>
     <br></br>
     <p><strong>El electrocardiograma es una herramienta diagnóstica que requiere la correlación clínica por parte del médico tratante</strong></p>
-    `));
+    `);
 
-    */
+    console.log(this.templateForm.value.textEditor);
   }
-
-
 
   cargarMultimedia() {
-    this.multimediaService.buscarPorOrdenVentaId(this.estudio.ordenVenta.id).subscribe(multimedia => {
-      this.multimedia = multimedia;
-      this.multimediaCargada = Promise.resolve(true);
-      console.log(multimedia);
-    },
-    error => {
-      console.log("Error al cargar multimedia");
-      Promise.resolve(false);
-    }
-    );
+    this.multimediaService
+      .buscarPorOrdenVentaId(this.estudio.ordenVenta.id)
+      .subscribe(
+        (multimedia) => {
+          this.multimedia = multimedia;
+          this.multimediaCargada = Promise.resolve(true);
+          console.log(multimedia);
+        },
+        (error) => {
+          console.log('Error al cargar multimedia');
+          Promise.resolve(false);
+        }
+      );
   }
-
 
   cargarAntecedentes() {
     this.antecedentes = '';
-    this.antecedenteEstudioService.filtrarPorVentaConceptosId(this.estudio.id).subscribe(a => a.forEach(antecedente => {
-      this.antecedentes += `${antecedente.antecedente.nombre} \n`;
-      if (this.estudio.concepto.area.nombre == 'CARDIOLOGIA') {
-        this.cargarPlantillaCardio();
-      }
-    },
-    error => {
-      console.log("Error al cargar antecedentes");
-    }));
+    this.antecedenteEstudioService
+      .filtrarPorVentaConceptosId(this.estudio.id)
+      .subscribe((a) =>
+        a.forEach(
+          (antecedente) => {
+            this.antecedentes += `${antecedente.antecedente.nombre} \n`;
+            if (this.estudio.concepto.area.nombre == 'CARDIOLOGIA') {
+              this.cargarPlantillaCardio();
+            }
+          },
+          (error) => {
+            console.log('Error al cargar antecedentes');
+          }
+        )
+      );
   }
 
   cargarAntecedentesInicial() {
-    this.antecedenteEstudioService.filtrarPorVentaConceptosId(this.estudio.id).subscribe(a => a.forEach(antecedente => {
-      this.antecedentes += `${antecedente.antecedente.nombre} \n`;
-      console.log("Cargando antecedentes iniciales");
-    },
-    error => {
-      console.log("Error al cargar antecedentes iniciales");
-    }));
-
+    this.antecedenteEstudioService
+      .filtrarPorVentaConceptosId(this.estudio.id)
+      .subscribe((a) =>
+        a.forEach(
+          (antecedente) => {
+            this.antecedentes += `${antecedente.antecedente.nombre} \n`;
+            console.log('Cargando antecedentes iniciales');
+          },
+          (error) => {
+            console.log('Error al cargar antecedentes iniciales');
+          }
+        )
+      );
   }
 
   ver(estudio: VentaConceptos): void {
@@ -158,7 +175,9 @@ export class DictadorComponent implements OnInit {
   }
 
   descargarZip(estudio: VentaConceptos): void {
-    window.open(`${ZIP_STUDIES_PATH}/${estudio.iuid}/?accept=application/zip;transfer-syntax=*`);
+    window.open(
+      `${ZIP_STUDIES_PATH}/${estudio.iuid}/?accept=application/zip;transfer-syntax=*`
+    );
   }
 
   abrirConWeasis(estudio: VentaConceptos): void {
@@ -174,72 +193,87 @@ export class DictadorComponent implements OnInit {
   }
 
   guardar() {
-      this.interpretacion = new Interpretacion();
-      this.enlacePdf = "";
-      this.interpretacion.estudio = this.estudio;
-      this.interpretacion.interpretacion = this.templateForm.value.textEditor;
+    this.interpretacion = new Interpretacion();
+    this.enlacePdf = '';
+    this.interpretacion.estudio = this.estudio;
+    this.interpretacion.interpretacion = this.templateForm.value.textEditor;
 
-      this.interpretacionService.crear(this.interpretacion).subscribe(
-        interpretacion => {
-          this.interpretacion = interpretacion;
-          this.enlacePdf = `${BASE_ENDPOINT}/ris/interpretaciones/estudio/${this.estudio.id}/pdf`;
-        },
-        () => {
-          console.log("Error creando la interpretación");
-        }
-      );
-
-      //this.enviarAvisoInterpretacionHechaACorreo();
-
-    // this.marcarEstudiosDeOrdenInterpretados();
-
-      //this.router.navigate(['/medico-radiologo/' + this.estudio.medicoRadiologo.token]);
-
+    this.interpretacionService.crear(this.interpretacion).subscribe(
+      (interpretacion) => {
+        this.interpretacion = interpretacion;
+        this.enlacePdf = `${BASE_ENDPOINT}/ris/interpretaciones/estudio/${this.estudio.id}/pdf`;
+      },
+      () => {
+        console.log('Error creando la interpretación');
+      }
+    );
   }
 
   private marcarEstudiosDeOrdenInterpretados() {
-    this.estudiosDeOrden.forEach(estudio => {
-      estudio.estado = "INTERPRETADO";
+    this.estudiosDeOrden.forEach((estudio) => {
+      estudio.estado = 'INTERPRETADO';
       this.actualizarEstudio(estudio);
     });
   }
 
   actualizarEstudio(estudio: VentaConceptos) {
-    this.ventaConceptosService.editar(estudio).subscribe(std => {
-      console.log("Estudio actualizado");
-    }, e => {
-      console.log("Error al actualizar estudio");
-    });
+    this.ventaConceptosService.editar(estudio).subscribe(
+      (std) => {
+        console.log('Estudio actualizado');
+      },
+      (e) => {
+        console.log('Error al actualizar estudio');
+      }
+    );
   }
 
   expandir(multimedia: Multimedia) {
-    this.multimediaService.verDocumento(multimedia).subscribe(res => {
+    this.multimediaService.verDocumento(multimedia).subscribe((res) => {
       const fileURL = URL.createObjectURL(res);
       window.open(fileURL, '_blank');
     });
   }
 
-
-
-private enviarAvisoInterpretacionHechaACorreo(): void  {
-  this.mailService.enviarAvisoInterpretacionHecha(this.estudio).subscribe(res =>{
-    console.log("Correo enviado");
-  },error =>{
-    console.log("Ha ocurrido un error al enviar el correo");
-  });
-}
-
-cargarEstudiosDeOrden(): void {
-  this.ventaConceptosService.encontrarPorOrdenVentaId(this.estudio.ordenVenta.id).subscribe(estudios => {
-    this.estudiosDeOrden = estudios.filter(estudio => estudio.medicoRadiologo.id === this.estudio.medicoRadiologo.id);
-  }, error =>{
-    console.log("Ha ocurrido un error al cargar estudios de la órden en cuestión");
-  });
-}
-
-
-  descargarPdf(): void{
-    window.open(`${BASE_ENDPOINT}/ris/interpretaciones/estudio/${this.estudio.id}/pdf`);
+  private enviarAvisoInterpretacionHechaACorreo(): void {
+    this.mailService.enviarAvisoInterpretacionHecha(this.estudio).subscribe(
+      (res) => {
+        console.log('Correo enviado');
+      },
+      (error) => {
+        console.log('Ha ocurrido un error al enviar el correo');
+      }
+    );
   }
 
+  cargarEstudiosDeOrden(): void {
+    this.ventaConceptosService
+      .encontrarPorOrdenVentaId(this.estudio.ordenVenta.id)
+      .subscribe(
+        (estudios) => {
+          this.estudiosDeOrden = estudios.filter(
+            (estudio) =>
+              estudio.medicoRadiologo.id === this.estudio.medicoRadiologo.id
+          );
+        },
+        (error) => {
+          console.log(
+            'Ha ocurrido un error al cargar estudios de la órden en cuestión'
+          );
+        }
+      );
+  }
+
+  firmar(): void {
+    this.enviarAvisoInterpretacionHechaACorreo();
+
+    this.marcarEstudiosDeOrdenInterpretados();
+
+    this.router.navigate([
+      '/medico-radiologo/' + this.estudio.medicoRadiologo.token,
+    ]);
+  }
+
+  /*descargarPdf(): void{
+    window.open(`${BASE_ENDPOINT}/ris/interpretaciones/estudio/${this.estudio.id}/pdf`);
+  }*/
 }
