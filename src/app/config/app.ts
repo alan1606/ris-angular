18b4a022dec6a0eaafe575f3a294411898f206ec
alan1606 <62415210+alan1606@users.noreply.github.com>
2023-11-16 18:38:41 -0600

    Modificado el enlace a subir foto orden

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -4,8 +4,6 @@ export const BASE_ENDPOINT = 'http://localhost:8090/api'
 export const VIEWER = 'https://ris.diagnocons.com:3322/viewer?StudyInstanceUIDs=';
 export const ZIP_STUDIES_PATH = 'https://ns1.diagnocons.com:8443/dcm4chee-arc/aets/DCM4CHEE/rs/studies';
 export const JPG_STUDIES_PATH = 'https://ris.diagnocons.com:8443/dcm4chee-arc/aets/DCM4CHEE/rs/studies/{studyInstanceUid}/series/{seriesNumber}/instances/{}/thumbnail';
-//Series generalmente es el 1 en rx, en tac es el topograma, instances es 1 o 2 en rx, en tac son muchas jaja
-//https://ris.diagnocons.com:8443/dcm4chee-arc/aets/DCM4CHEE/rs/studies/1.2.410.200067.100.1.202105271113050290.29133/series/1.2.410.200067.100.2.202105271113050290.32740.1/instances/1/thumbnail
 
 export const IMAGE_PATH = BASE_ENDPOINT + '/ris/multimedia/imagen/orden-venta/';
 export const PDF_PATH = BASE_ENDPOINT + '/ris/multimedia/documento/orden-venta/';
@@ -15,7 +13,7 @@ export const DOWNLOAD_WEASIS_WINDOWS_LINK = 'https://github.com/nroduit/Weasis/r
 export const DOWNLOAD_WEASIS_MAC_LINK = 'https://github.com/nroduit/Weasis/releases/download/v3.7.0/Weasis-3.7.0.pkg';
 export const RESULTS_URL = 'https://ris.diagnocons.com/ris/resultados/';
 export const DIRECCION_CORREO_CONS = 'diagnocons@gmail.com';
-export const URL_SUBIR_FOTO = 'https://ris.diagnocons.com/ris/subir-foto-orden/';
+export const URL_SUBIR_FOTO = 'https://ris.diagnocons.com/ris/recepcion/subir-foto-orden/';
 
 
 const BASE_SERVER_ADDRESS = 'https://auth.diagnocons.com/realms/diagnocons/protocol/openid-connect/';
