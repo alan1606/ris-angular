b8359e65d0f955cbe36bebf0f40c9df3d83a147a
lecheropro777 <juan.martinez3168@gmail.com>
2024-01-08 11:45:21 -0600

    diseño y render de datos terminado
    faltan los servicios para crear o editar al medico

--- src/app/components/crud-medicos/crud-medicos.component.ts
+++ src/app/components/crud-medicos/crud-medicos.component.ts
@@ -2,49 +2,67 @@ import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MatDialog } from '@angular/material/dialog';
-import { catchError, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+} from 'rxjs';
 import { Medico } from 'src/app/models/medico';
 import { MedicoService } from 'src/app/services/medico.service';
 import { FormularioMedicosComponent } from './formulario-medicos/formulario-medicos.component';
 
-
 @Component({
   selector: 'app-crud-medicos',
   templateUrl: './crud-medicos.component.html',
-  styleUrls: ['./crud-medicos.component.css']
+  styleUrls: ['./crud-medicos.component.css'],
 })
 export class CrudMedicosComponent implements OnInit {
-
   constructor(
-    private medicoService:MedicoService,
-    private dialog:MatDialog
-  ) {
-    
-   }
-  nombre="juan".toLowerCase();
-  autoCompleteControlMedicoRadiologo= new UntypedFormControl('');
-  autoCompleteControlMedicoReferente =new UntypedFormControl('');
-  medico:Medico=null;
+    private medicoService: MedicoService,
+    private dialog: MatDialog
+  ) {}
+  autoCompleteControlMedicoReferente = new UntypedFormControl('');
+  medico: Medico = null;
   medicosFiltradosRadiologo: Medico[] = [];
   medicosFiltradosReferente: Medico[] = [];
 
   ngOnInit(): void {
-    this.autoCompleteControlMedicoRadiologo.valueChanges.pipe(
-      debounceTime(300), 
-      distinctUntilChanged(), 
-      switchMap(valor => {
-        const nombresMedico = typeof valor === 'string' ? valor : valor?.nombres;
-        return valor ? this.medicoService.filtrarRadiologosPorNombre(nombresMedico) : [];
-      }),
-      catchError(error => {
-        console.error('Error en la búsqueda de médicos radiólogos:', error);
-        return [];
-      })
-    ).subscribe(medicos => {
-      this.medicosFiltradosRadiologo = medicos;
-    });
+    // this.autoCompleteControlMedicoReferente.valueChanges.pipe(
+    //   debounceTime(300),
+    //   distinctUntilChanged(),
+    //   switchMap(valor => {
+    //     const nombresMedico = typeof valor === 'string' ? valor : valor?.nombres;
+    //     return valor ? this.medicoService.filtrarRadiologosPorNombre(nombresMedico) : [];
+    //   }),
+    //   catchError(error => {
+    //     console.error('Error en la búsqueda de médicos radiólogos:', error);
+    //     return [];
+    //   })
+    // ).subscribe(medicos => {
+    //   this.medicosFiltradosReferente= medicos;
+    // });
+
+    this.autoCompleteControlMedicoReferente.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((valor) => {
+          const nombresMedico =
+            typeof valor === 'string' ? valor : valor?.nombres;
+          return valor
+            ? this.medicoService.filtrarReferentesPorNombre(nombresMedico)
+            : [];
+        }),
+        catchError((error) => {
+          console.error('Error en la búsqueda de médicos radiólogos:', error);
+          return [];
+        })
+      )
+      .subscribe((medicos) => {
+        this.medicosFiltradosReferente = medicos;
+      });
   }
-  
 
   mostrarNombreMedico(medico: Medico): string {
     return medico && medico.nombres ? medico.nombres : '';
@@ -55,11 +73,16 @@ export class CrudMedicosComponent implements OnInit {
     event.option.focus();
   }
 
-  abrirModalFormularioMedicos(medico?:Medico){
-    this.dialog.open(FormularioMedicosComponent,{
-      width:"1000px",
-      data:{medico:this.medico?this.medico:null}
-    }
-    )
+  abrirModalFormularioMedico() {
+    this.dialog.open(FormularioMedicosComponent, {
+      width: '1000px',
+    });
+  }
+
+  abrirModalFormularioEditarMedico(medico: Medico) {
+    this.dialog.open(FormularioMedicosComponent, {
+      width: '1000px',
+      data: { medicoDatos:medico},
+    });
   }
 }
--- src/app/components/crud-medicos/formulario-medicos/formulario-medicos.component.ts
+++ src/app/components/crud-medicos/formulario-medicos/formulario-medicos.component.ts
@@ -1,6 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  FormControl,
+  FormGroup,
+  RequiredValidator,
+  Validators,
+} from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Medico } from 'src/app/models/medico';
+import { FechaService } from 'src/app/services/fecha.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-formulario-medicos',
@@ -8,12 +16,95 @@ import { Medico } from 'src/app/models/medico';
   styleUrls: ['./formulario-medicos.component.css'],
 })
 export class FormularioMedicosComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
-  modelo=null;
-  medico: Medico = null;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fechaService: FechaService,
+    public modalRef: MatDialogRef<FormularioMedicosComponent>
+  ) {}
+  modelo = null;
+  medico: Medico;
+  error: any;
+  fecha: string = '';
+  form: FormGroup = new FormGroup({
+    Nombres: new FormControl('', Validators.required),
+    Apellidos: new FormControl('', Validators.required),
+    Especialidad: new FormControl(''),
+    Telefono: new FormControl(''),
+    Correo: new FormControl('', [Validators.required, Validators.email]),
+    Direccion: new FormControl(''),
+    FechaNacimiento: new FormControl(''),
+    Whatsapp: new FormControl('', [Validators.minLength(10)]),
+    Sexo: new FormControl(''),
+  });
+  sexos = [
+    { label: 'Femenino', value: 1 },
+    { label: 'Masculino', value: 2 },
+  ];
+
   ngOnInit(): void {
     if (this.data) {
-      this.medico = this.data.medico;
+      this.medico = this.data.medicoDatos;
+      console.log(this.medico);
+      this.form.patchValue({
+        Nombres: this.medico.nombres,
+        Apellidos: this.medico.apellidos,
+        Especialidad: this.medico.especialidad,
+        Telefono: this.medico.telefono,
+        Correo: this.medico.correo,
+        Direccion: this.medico.direccion,
+        FechaNacimiento: this.medico.fechaNacimiento,
+        Sexo: this.medico.sexo == 1 ? 1 : 2,
+      });
     }
+  }
+
+  seleccionarFecha(fecha: HTMLInputElement): void {
+    this.fecha = this.fechaService.alistarFechaParaBackend(fecha.value);
+    this.form.value.FechaNacimiento = this.fecha;
+    // this.form.value.FechaNacimiento += 'T00:00:00';
+  }
+
+  Crear(): boolean {
+    if (!this.form.value.Nombres) {
+      Swal.fire('Error', 'Llenar nombre', 'error');
+      return false;
+    }
+    if (!this.form.value.Apellidos) {
+      Swal.fire('Error', 'Llenar apellidos', 'error');
+      return false;
+    }
+    if (!this.form.value.Correo || this.form.value.Correo.indexOf('@') === -1) {
+      Swal.fire('Error', 'Llenar correo', 'error');
+      return false;
+    }
+    console.log('creado');
+    this.medico = this.form.value;
+    console.log(this.medico);
+    Swal.fire('Creado', 'Nuevo medico creado', 'success');
+    return true;
+  }
+
+  Editar(): boolean {
+    if (!this.form.value.Nombres) {
+      Swal.fire('Error', 'Llenar nombre', 'error');
+      return false;
+    }
+    if (!this.form.value.Apellidos) {
+      Swal.fire('Error', 'Llenar apellidos', 'error');
+      return false;
+    }
+    if (!this.form.value.Correo || this.form.value.Correo.indexOf('@') === -1) {
+      Swal.fire('Error', 'Llenar correo', 'error');
+      return false;
+    }
+    console.log('editado');
+    this.medico = this.form.value;
+    console.log(this.medico);
+    Swal.fire('Editado', 'Medico editado', 'success');
+    return true;
+  }
+
+  Cerrar(): void {
+    this.modalRef.close();
   }
 }
