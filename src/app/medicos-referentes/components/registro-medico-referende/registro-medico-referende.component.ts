10eeae5326781e66c4f2c42e8269aacbb9dfa9a0
lecheropro777 <juan.martinez3168@gmail.com>
2024-02-10 10:28:58 -0600

    componente de landing no terminado, componente de registro medico-referente casi terminado falta backend para continuar

--- src/app/medicos-referentes/components/registro-medico-referende/registro-medico-referende.component.ts
+++ src/app/medicos-referentes/components/registro-medico-referende/registro-medico-referende.component.ts
@@ -1,27 +1,79 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  EmailValidator,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { title } from 'process';
+import { MedicoService } from 'src/app/services/medico.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-registro-medico-referende',
   templateUrl: './registro-medico-referende.component.html',
-  styleUrls: ['./registro-medico-referende.component.css']
+  styleUrls: ['./registro-medico-referende.component.css'],
 })
-export class RegistroMedicoReferendeComponent {
+export class RegistroMedicoReferendeComponent implements OnInit {
+  correoElectronicoInput: FormControl<string> = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  correoElectronico: String = null;
+  correosEncontrados: String[] = null;
+  codigoVerificacion: FormControl<string> = new FormControl(null);
 
-  form:FormGroup = new FormGroup({
-    Usuario: new FormControl(""),
-    Password: new FormControl(""),
-    ConfirmPassword: new FormControl(""),
-    Telefono: new FormControl(""),
-    Direccion: new FormControl("")
+  form: FormGroup = new FormGroup({
+    Usuario: new FormControl(''),
+    Password: new FormControl(''),
+    ConfirmPassword: new FormControl(''),
+    Telefono: new FormControl(''),
+    Direccion: new FormControl(''),
   });
 
+  constructor(private medicoService: MedicoService) {}
 
-  registrarUsuario(){
+  ngOnInit(): void {}
+
+  enviarCorreoVerificacion() {
+    this.correoElectronico = this.correoElectronicoInput.value;
+    if (this.correoElectronico) {
+      this.medicoService.enviarCorreoVerificacion(this.correoElectronico);
+    }
+    return;
+    // this.correosEncontrados=[]
+    // let busqueda = this.medicoService.encontrarMedicoReferentePorCorreo(this.correoElectronicoInput.value);
+
+    // if (busqueda) {
+    //   if (this.correosEncontrados[0] === busqueda) {
+    //     this.correoElectronico = null;
+    //     return;
+    //   }
+    //   this.correosEncontrados.push(busqueda);
+    //   this.correoElectronico = null;
+    // }
+    // this.correoElectronico = this.correoElectronicoInput.value;
+    // return;
+  }
+
+  enviarCodigoVerificacion() {
+    const res = this.medicoService.verificarCodigo(
+      this.codigoVerificacion.value
+    );
+    if (!res) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'error',
+      });
+    }
+    this.correosEncontrados = res;
+  }
+
+  registrarUsuario() {
     console.log(this.form.value);
   }
 
-  mostraUsuario(e){
+  mostraUsuario(e) {
     console.log(e);
   }
 }
--- src/app/services/medico.service.ts
+++ src/app/services/medico.service.ts
@@ -6,45 +6,94 @@ import { Medico } from '../models/medico';
 import { CommonService } from './common.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class MedicoService { 
-  private baseEndpoint = BASE_ENDPOINT +  '/ris/medicos';
-  private cabeceras: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+export class MedicoService {
+  private baseEndpoint = BASE_ENDPOINT + '/ris/medicos';
+  private cabeceras: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
 
-  constructor(private http: HttpClient) {   }
+  constructor(private http: HttpClient) {}
 
-  public filtrarRadiologosPorNombre(nombre: string): Observable<Medico[]>{
-    return this.http.get<Medico[]>(`${this.baseEndpoint}/radiologos/nombre/${nombre}`);
+  public filtrarRadiologosPorNombre(nombre: string): Observable<Medico[]> {
+    return this.http.get<Medico[]>(
+      `${this.baseEndpoint}/radiologos/nombre/${nombre}`
+    );
   }
 
-  public filtrarReferentesPorNombre(nombre: string): Observable<Medico[]>{
-    return this.http.get<Medico[]>(`${this.baseEndpoint}/referentes/nombre/${nombre}`);
+  public filtrarReferentesPorNombre(nombre: string): Observable<Medico[]> {
+    return this.http.get<Medico[]>(
+      `${this.baseEndpoint}/referentes/nombre/${nombre}`
+    );
   }
 
-  public encontrarRadiologoPorToken(token: string): Observable<Medico>{
-    return this.http.get<Medico>((`${this.baseEndpoint}/radiologo/token/${token}`));
+  public encontrarRadiologoPorToken(token: string): Observable<Medico> {
+    return this.http.get<Medico>(
+      `${this.baseEndpoint}/radiologo/token/${token}`
+    );
   }
-  
-  public encontrarMedicoPorTokenPorUsuario(usuario: string): Observable<Medico>{
-    return this.http.get<Medico>(`${this.baseEndpoint}/token/usuario/${usuario}`);
+
+  public encontrarMedicoPorTokenPorUsuario(
+    usuario: string
+  ): Observable<Medico> {
+    return this.http.get<Medico>(
+      `${this.baseEndpoint}/token/usuario/${usuario}`
+    );
   }
 
   //Editar ya está en backend
-  public editar(medico: Medico): Observable<Medico>{
-    return this.http.put<Medico>(`${this.baseEndpoint}/${medico.id}`, medico,
-    { headers: this.cabeceras });
+  public editar(medico: Medico): Observable<Medico> {
+    return this.http.put<Medico>(`${this.baseEndpoint}/${medico.id}`, medico, {
+      headers: this.cabeceras,
+    });
   }
 
   //crear referente ya está en el backend
-  public crearMedicoReferente(medico: Medico): Observable<Medico>{
-    return this.http.post<Medico>(`${this.baseEndpoint}/referentes`, medico,
-    { headers: this.cabeceras });
+  public crearMedicoReferente(medico: Medico): Observable<Medico> {
+    return this.http.post<Medico>(`${this.baseEndpoint}/referentes`, medico, {
+      headers: this.cabeceras,
+    });
   }
 
   public crearMedicoReferentePurosNombre(medico: Medico): Observable<Medico> {
-    return this.http.post<Medico>(`${this.baseEndpoint}/referentes-nombre`, medico,
-    { headers: this.cabeceras });
+    return this.http.post<Medico>(
+      `${this.baseEndpoint}/referentes-nombre`,
+      medico,
+      { headers: this.cabeceras }
+    );
   }
 
+  public encontrarMedicoReferentePorCorreo(correoElectronico: String): String {
+    let correos: String[] = [
+      'juan@gmail.com',
+      'juan@gmail.com',
+      'juan@gmail.com',
+      'alan@gmail.com',
+      'emmanuel@gmail.com',
+    ];
+    let busqueda = correos.includes(correoElectronico, 0);
+    if (busqueda) {
+      return correoElectronico;
+    }
+    return null;
+  }
+  public enviarCorreoVerificacion(correo: String): String {
+    console.log(`Codigo enviado al correo ${correo}`);
+    return 'Codigo enviado';
+  }
+
+  public verificarCodigo(codigo: String): String[] {
+    let correos: String[] = [
+      'juan@gmail.com',
+      'juan@gmail.com',
+      'juan@gmail.com',
+      'alan@gmail.com',
+      'emmanuel@gmail.com',
+    ];
+    if (codigo === '777') {
+      return correos;
+    }
+    return null;
+  }
 }
