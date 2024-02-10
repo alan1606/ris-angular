d1f23a6f51ac25ba2eabcaebf54a44113e240ce5
lecheropro777 <juan.martinez3168@gmail.com>
2024-02-10 12:40:01 -0600

    creacion del modulo y componente ayuda, falta adjuntar videos explicativos

---
+++ src/app/ayuda/ayuda.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { AyudaRoutingModule } from './ayuda-routing.module';
+import { AyudaComponent } from './components/ayuda/ayuda.component';
+
+@NgModule({
+  declarations: [AyudaComponent],
+  imports: [CommonModule, AyudaRoutingModule, MatExpansionModule],
+})
+export class AyudaModule {}
