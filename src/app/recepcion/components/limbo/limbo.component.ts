fce986c344b11142b5c6b732297cd1d1869fca35
lecheropro777 <juan.martinez3168@gmail.com>
2024-02-14 13:18:24 -0600

    limbo iniciado

---
+++ src/app/recepcion/components/limbo/limbo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-limbo',
+  templateUrl: './limbo.component.html',
+  styleUrls: ['./limbo.component.css']
+})
+export class LimboComponent {
+
+}
