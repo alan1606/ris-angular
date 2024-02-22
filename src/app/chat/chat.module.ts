150c630666c76c8266faba00685b1e0f5ca040f4
lecheropro777 <juan.martinez3168@gmail.com>
2024-02-22 12:16:57 -0600

    diseño del chat mejorado, haber que les parece

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ChatRoutingModule } from './chat-routing.module';
 import { PrincipalComponent } from './components/principal/principal.component';
-import { SideBarChatsComponent } from './components/side-bar-chats/side-bar-chats.component';
-import { ChatComponent } from './components/chat/chat.component';
-  
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    PrincipalComponent,
-    SideBarChatsComponent,
-    ChatComponent
+    PrincipalComponent
     ],
   imports: [
     CommonModule,
-    ChatRoutingModule
+    ChatRoutingModule,
+    FormsModule
   ]
 })
 export class ChatModule { }
--- src/app/chat/components/principal/principal.component.ts
+++ src/app/chat/components/principal/principal.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ChatService } from '../../services/chat.service';
 import { Message } from '@stomp/stompjs';
 
@@ -7,53 +13,65 @@ import { Message } from '@stomp/stompjs';
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css'],
 })
-export class PrincipalComponent implements OnInit{
-  //telefono: string = '6271437428';
-
-  chats: string[] = [];
-  chatSeleccionado = "";
-
-  listaMensajes: Message[] = [];
-
-
-  constructor(private chatService: ChatService){
+export class PrincipalComponent implements OnInit, AfterViewInit {
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+  constructor(private chatService: ChatService) {
     this.chatService.initConnenctionSocket();
   }
+  chats: string[] = [];
+  chatSeleccionado = '';
+  busqueda: string = null;
+  chatsFiltrados: string[] = [];
+  listaMensajes: Message[] = [];
 
   ngOnInit(): void {
     this.chatService.joinWpTopic();
     this.listenerMessage();
     this.buscarChats();
   }
+  ngAfterViewInit() {
+    this.scrollToBottom();
+  }
 
   listenerMessage() {
     this.chatService.getMessageSubject().subscribe((mensaje: any) => {
       //Si recibido.phone == chatSeleccionado, meter mensaje nuevo, si no, mover lista de chats
-      if(mensaje.phoneNumber == this.chatSeleccionado){
+      if (mensaje.phoneNumber == this.chatSeleccionado) {
         this.listaMensajes.push(mensaje);
-      }
-      else{
-        this.chats = this.chats.filter(chat => chat!=mensaje.phoneNumber);
+      } else {
+        this.chats = this.chats.filter((chat) => chat != mensaje.phoneNumber);
         this.chats = [mensaje.phoneNumber, ...this.chats];
       }
     });
   }
+  filtrarChats() {
+    this.chatsFiltrados = !this.busqueda
+      ? this.chats
+      : this.chats.filter((chat) => chat.includes(this.busqueda));
+  }
 
-  private buscarChats(){
-    this.chatService.getContactsList().subscribe(contacts => {
+  private buscarChats() {
+    this.chatService.getContactsList().subscribe((contacts) => {
       this.chats = contacts;
+      this.chatsFiltrados = contacts;
     });
   }
 
-  entrarAlChat(chat: string){
-    if(this.chatSeleccionado == chat){
+  entrarAlChat(chat: string) {
+    console.log(chat);
+    if (this.chatSeleccionado == chat) {
       return;
       //No buscar mensajes que ya están cargados
     }
     this.chatSeleccionado = chat;
-    this.chatService.getMessagesByPhoneNumber(chat).subscribe(mensajes =>{
+    this.chatService.getMessagesByPhoneNumber(chat).subscribe((mensajes) => {
       this.listaMensajes = mensajes;
+      console.log(this.listaMensajes);
     });
   }
 
+  scrollToBottom() {
+    const container = this.scrollContainer.nativeElement;
+    container.scrollTop = container.scrollHeight - container.clientHeight;
+  }
 }
