b11774a1e2734dd1e3da5ff81c13c87566405a3a
alan1606 <62415210+alan1606@users.noreply.github.com>
2024-03-14 20:06:57 -0600

    Movida landing medico

--- src/app/medicos-referentes/components/registro-medico-referende/registro-medico-referende.component.ts
+++ src/app/medicos-referentes/components/registro-medico-referende/registro-medico-referende.component.ts
@@ -14,16 +14,8 @@ export class RegistroMedicoReferendeComponent {
   codigoVerficiacion: FormControl = new FormControl(null);
   verificado: boolean = false;
 
-  
 
   enviarCodigoVerificacion() {
-    if (this.whatsapp.value && this.correo.value) {
-      Swal.fire({
-        icon: 'info',
-        title: 'Solo llene uno de los dos campos',
-      });
-      return;
-    }
     if (!this.correo.value && !this.whatsapp.value) {
       Swal.fire({
         icon: 'info',
@@ -31,32 +23,35 @@ export class RegistroMedicoReferendeComponent {
       });
       return;
     }
-    if (!this.whatsapp.value) {
-      console.log('Correo enviado: ' + this.correo.value);
+    if (this.whatsapp.value && this.correo.value) {
+
+      return;
+    }
+    if (this.whatsapp.value) {
+      console.log('Whatsapp enviado: ' + this.whatsapp.value);
       this.codigoEnviado = true;
     }
-    if (!this.correo.value) {
-      console.log('Whatsapp enviado: ' + this.whatsapp.value);
+    if (this.correo.value) {
+      console.log('Correo enviado: ' + this.correo.value);
       this.codigoEnviado = true;
     }
   }
 
-  verificarCodgio(): boolean {
+  verificarCodgio() {
     if (!this.codigoVerficiacion.value) {
       Swal.fire({
         icon: 'error',
-        title: 'Escriba su codigo profavor',
+        title: 'Escriba su código, por favor',
       });
     }
     if (this.codigoVerficiacion.value) {
-      console.log('Verificando');
       Swal.fire({
         icon: 'success',
         title: 'Verificado correctamente',
       });
-      return (this.verificado = true);
+       this.verificado = true;
     }
-    return false;
+    this.verificado = false;
   }
 
   recibirDatosUsuario(event){
