2efccb85340b287a7e60fd17eceb89607254592a
lecheropro777 <juan.martinez3168@gmail.com>
2024-03-15 15:55:27 -0600

    cambios en alertas y prueba para nestjs

--- src/app/medicos-referentes/components/registro-medico-referende/registro-medico-referende.component.ts
+++ src/app/medicos-referentes/components/registro-medico-referende/registro-medico-referende.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { MedicoReferenteService } from '../../services/medico-referente.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-registro-medico-referende',
   templateUrl: './registro-medico-referende.component.html',
   styleUrls: ['./registro-medico-referende.component.css'],
 })
-export class RegistroMedicoReferendeComponent {
+export class RegistroMedicoReferendeComponent implements OnInit {
+  constructor(private medicosReferentesServiceNestjs: MedicoReferenteService) {}
+
   whatsapp: FormControl = new FormControl(null);
   correo: FormControl = new FormControl(null, [Validators.email]);
   codigoEnviado: boolean = false;
   codigoVerficiacion: FormControl = new FormControl(null);
   verificado: boolean = false;
 
-
+  ngOnInit(): void {
+    this.medicosReferentesServiceNestjs
+      .obtenerMedicosReferentesNestjs()
+      .subscribe(
+        (datos) => console.log(datos),
+        (error) => console.log(error)
+      );
+  }
   enviarCodigoVerificacion() {
     if (!this.correo.value && !this.whatsapp.value) {
       Swal.fire({
@@ -24,16 +35,27 @@ export class RegistroMedicoReferendeComponent {
       return;
     }
     if (this.whatsapp.value && this.correo.value) {
-
       return;
     }
     if (this.whatsapp.value) {
       console.log('Whatsapp enviado: ' + this.whatsapp.value);
+      Swal.fire({
+        icon: 'success',
+        title: `Codigo de verificacion enviado a whatsapp`,
+        text: 'Puede tardar un momento en llegar',
+      });
       this.codigoEnviado = true;
+      return;
     }
     if (this.correo.value) {
       console.log('Correo enviado: ' + this.correo.value);
+      Swal.fire({
+        icon: 'success',
+        title: `Codigo de verificacion enviado al correo`,
+        text: 'Puede tardar un momento en llegar',
+      });
       this.codigoEnviado = true;
+      return;
     }
   }
 
@@ -43,21 +65,21 @@ export class RegistroMedicoReferendeComponent {
         icon: 'error',
         title: 'Escriba su código, por favor',
       });
+      return (this.verificado = false);
     }
     if (this.codigoVerficiacion.value) {
       Swal.fire({
         icon: 'success',
         title: 'Verificado correctamente',
       });
-       this.verificado = true;
+      return (this.verificado = true);
     }
-    this.verificado = false;
+    return (this.verificado = false);
   }
 
-  recibirDatosUsuario(event){
-    console.log(event)
+  recibirDatosUsuario(event) {
+    console.log(event);
   }
-
 
   mostraUsuario(e) {
     console.log(e);
--- src/app/medicos-referentes/services/medico-referente.service.ts
+++ src/app/medicos-referentes/services/medico-referente.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Medico } from 'src/app/models/medico';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoReferenteService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  obtenerMedicosReferentesNestjs():Observable<Medico[]>{
+    return this.http.get<Medico[]>("http://localhost:3000/medicos-referentes")
+  }
 }
