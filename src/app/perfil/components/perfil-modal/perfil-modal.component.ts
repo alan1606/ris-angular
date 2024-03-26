4abe1346c87fd6fbd88ec027dfb9b80335306ab4
lecheropro777 <juan.martinez3168@gmail.com>
2024-03-26 13:39:37 -0600

    obtengo datos dinamicos de los perfiles de medicos referentes, pero solo sirve para los que son medicos referentes

--- src/app/perfil/components/perfil-modal/perfil-modal.component.ts
+++ src/app/perfil/components/perfil-modal/perfil-modal.component.ts
@@ -1,50 +1,68 @@
+import { DatePipe } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, UntypedFormControl } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Medico } from 'src/app/models/medico';
 import { FechaService } from 'src/app/services/fecha.service';
+import { MedicoService } from 'src/app/services/medico.service';
+import { TokenService } from 'src/app/services/token.service';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-perfil-modal',
   templateUrl: './perfil-modal.component.html',
-  styleUrls: ['./perfil-modal.component.css']
+  styleUrls: ['./perfil-modal.component.css'],
 })
 export class PerfilModalComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fechaService: FechaService,
+    private medicosService: MedicoService,
+    private tokenService: TokenService,
+    private pipe: DatePipe
+  ) {}
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private fechaService:FechaService){}
-
-  username:string=''
-  correo:string='juan@gmail.com'
-  whatsapp:string='6271437428'
-  direccion:string='Pintores mexicanos #53'
-  fechaNacimiento:FormControl= new FormControl()
-  sexos:string[]=["1","2"]
-  sexo:string="2"
-  editarCampos:boolean=true
+  editarCampos: boolean = true;
+  datosUsuario: Medico = new Medico();
+  username: string = '';
+  fechaNacimientoControl: FormControl = new FormControl({
+    value: '',
+    disabled: this.editarCampos,
+  });
+  fecha: string = '';
+  sexos: string[] = ['1', '2'];
 
   ngOnInit(): void {
-    this.username=this.data.user
-    this.editarCampos=false
-    this.fechaNacimiento.setValue(new Date("2003-04-08T00:00:00"))
-    this.editarCampos=true
-
+    this.username = this.tokenService.getUsername();
+    this.medicosService.encontrarPorUsuario(this.username).subscribe(
+      (datos) => {
+        this.datosUsuario = datos;
+        this.fechaNacimientoControl.setValue(
+          new Date((datos.fechaNacimiento += 'T00:00:00'))
+        );
+      },
+      (error) => console.log(error)
+    );
   }
 
-  cambiarEditar(){
-    this.editarCampos=!this.editarCampos
+  cambiarEditar() {
+    this.editarCampos = !this.editarCampos;
   }
 
   public actualizarFecha(fecha: HTMLInputElement) {
-    console.log(fecha.value)
-  };
+    console.log(fecha.value);
+  }
 
-  guardarCambios(){
-    console.log(this.sexo)
-    console.log(this.fechaNacimiento.value)
+  seleccionarFecha(): void {
+    const fechaValor = new Date(this.fechaNacimientoControl.value);
+    this.datosUsuario.fechaNacimiento =this.fechaService.formatearFecha(fechaValor);
+    this.datosUsuario.fechaNacimiento += 'T00:00:00';
+  }
+  guardarCambios() {
     Swal.fire({
-      icon:"success",
-      title:"Datos guardados"
-    })
-    this.editarCampos=true
+      icon: 'success',
+      title: 'Datos guardados',
+    });
+    this.editarCampos = true;
   }
 }
