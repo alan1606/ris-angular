7210b657bc94f0c08635ea1340c06fc3cabad0a3
Administrator <juan.martinez3168@gmail.com>
2024-09-24 07:54:08 -0600

    Permite a las recepcionistas abrir el venta conceptos general

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   BASE_SITE: 'http://localhost:4200/ris',
-  BASE_ENDPOINT: 'http://172.17.200.235:8090/api',
+  BASE_ENDPOINT: 'https://ris.diagnocons.com/api',
   post_logout_redirect_uri: 'http://localhost:4200/ris/logout',
   redirect_uri: 'http://localhost:4200/ris/authorized?method=get',
   turneroSocket: 'http://172.17.200.235:8002/shifts-websocket',
