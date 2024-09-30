ca83bc65488974414875512b569c90ae40fe1314
Administrator <juan.martinez3168@gmail.com>
2024-09-30 12:59:25 -0600

    Ahorita la logica funciona bien, si el proceso en el turnero se sigue correctamente, hace mas pruebas

--- src/app/pantallas/components/ver-pantallas/ver-pantallas.component.ts
+++ src/app/pantallas/components/ver-pantallas/ver-pantallas.component.ts
@@ -1,13 +1,9 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { TurneroSubscription } from 'src/app/models/TurneroSubscription';
-import { TokenService } from 'src/app/services/token.service';
-import { AlertaService } from 'src/app/shared/services/alerta.service';
 import { TurneroSocketService } from 'src/app/turnero/services/turnero-socket.service';
-import { TurneroService } from 'src/app/turnero/services/turnero.service';
 import { DicomRoom } from '../../models/DicomRoom';
 import { PantallasService } from '../../services/pantallas.service';
 import { Pantalla } from '../../models/Pantalla';
-import { da } from 'date-fns/locale';
 
 @Component({
   selector: 'app-ver-pantallas',
@@ -15,43 +11,29 @@ import { da } from 'date-fns/locale';
   styleUrl: './ver-pantallas.component.css',
 })
 export class VerPantallasComponent implements OnInit {
-  private alertaService = inject(AlertaService);
   private pantallasService = inject(PantallasService);
-  private turneroService = inject(TurneroService);
   private turneroSocketService = inject(TurneroSocketService);
-  private tokenService = inject(TokenService);
-  private user = this.tokenService.getUsername();
   public subscriptions = signal<TurneroSubscription[]>([]);
   public estudios: any[] = [];
   public salas: DicomRoom[] = [];
-  public turnos: Pantalla[] = [];
+  public waitingTurns: Pantalla[] = [];
   public pasar: Pantalla[] = [];
 
   ngOnInit(): void {
     this.pantallasService.findAllEnabled().subscribe(
       (data) => {
         this.salas = data;
-        this.turnos = this.salas as Pantalla[];
+        this.waitingTurns = this.salas as Pantalla[];
         this.pasar = this.salas as Pantalla[];
         const turnosGuardados = localStorage.getItem('Turnos');
         const pasarGuardados = localStorage.getItem('Pasar');
 
         if (turnosGuardados) {
-          this.turnos = JSON.parse(turnosGuardados) as Pantalla[];
+          this.waitingTurns = JSON.parse(turnosGuardados) as Pantalla[];
         }
         if (pasarGuardados) {
           this.pasar = JSON.parse(pasarGuardados) as Pantalla[];
         }
-
-        // this.pasar.forEach((p) => {
-        //   p.turnos.forEach((pturno) => {
-        //     this.turnos.forEach((t) => {
-        //       t.turnos = t.turnos.filter(
-        //         (l) => l.idToDisplay !== pturno.idToDisplay
-        //       );
-        //     });
-        //   });
-        // });
       },
       (error) => console.log(error)
     );
@@ -66,6 +48,7 @@ export class VerPantallasComponent implements OnInit {
         if (data?.idToDisplay && !data.arrivedTime) {
           console.log('entro el if pasar');
           this.showTurnToPass(data);
+          this.deletePassingStudies(data);
           return;
         }
 
@@ -77,13 +60,15 @@ export class VerPantallasComponent implements OnInit {
   }
 
   private addTurn(data: any): void {
-    let salaPantalla = this.turnos.find((sala) => sala.id === data.dicomRoomId);
+    let salaPantalla = this.waitingTurns.find(
+      (sala) => sala.id === data.dicomRoomId
+    );
     if (salaPantalla) {
       if (!salaPantalla.turnos) {
         salaPantalla.turnos = [];
       }
       salaPantalla.turnos.unshift(data);
-      localStorage.setItem('Turnos', JSON.stringify(this.turnos));
+      localStorage.setItem('Turnos', JSON.stringify(this.waitingTurns));
     }
   }
 
@@ -96,5 +81,14 @@ export class VerPantallasComponent implements OnInit {
       pasarPantalla.turnos[0] = data;
       localStorage.setItem('Pasar', JSON.stringify(this.pasar));
     }
+  }
+
+  private deletePassingStudies(data: any): void {
+    this.waitingTurns.forEach((t) => {
+      if (Array.isArray(t.turnos)) {
+        t.turnos = t.turnos.filter((l) => l.idToDisplay !== data.idToDisplay);
+      }
+    });
+    localStorage.setItem('Turnos', JSON.stringify(this.waitingTurns));
   }
 }
